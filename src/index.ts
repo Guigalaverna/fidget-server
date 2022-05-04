d270cc3021d0dd6df2be143ba6f4e7c912e760eb
Guigalaverna <guigalaverna@gmail.com>
2022-05-04 16:30:38 -0300

    feat: creating feedback

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,15 @@
 import express from 'express'
 import 'dotenv/config'
+import { FeedbacksController } from './controllers/Feedbacks'
 
 const app = express()
 const PORT = process.env.PORT || 3333
+
+app.use(express.json())
+
+const controller = new FeedbacksController()
+
+app.post('/feedback/create', controller.handle)
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`)
